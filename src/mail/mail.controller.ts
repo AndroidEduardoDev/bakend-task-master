2895420ec055e635691df963757975307bc42627
Eduardo Fonseca <bredgrfo@gmail.com>
2023-07-28 21:07:37 -0500

    email recover

---
+++ src/mail/mail.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { UserCreateDto } from 'src/users/dto/user.create.dto';
+
+@Controller('mail')
+export class MailController {
+
+    constructor(private mailService: MailService) { }
+
+    @Post("/recover")
+    async signUp(@Body() user: UserCreateDto) {
+        const token = Math.floor(1000 + Math.random() * 9000).toString();
+        await this.mailService.sendUserConfirmation(user, token);
+    }
+}
---
+++ src/mail/mail.service.ts
@@ -0,0 +1,24 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
+import { UserCreateDto } from 'src/users/dto/user.create.dto';
+import { Users } from 'src/users/model/users.entity';
+
+@Injectable()
+export class MailService {
+    constructor(private mailerService: MailerService) { }
+
+    async sendUserConfirmation(user: UserCreateDto, token: string) {
+        const url = `example.com/auth/confirm?token=${token}`;
+
+        await this.mailerService.sendMail({
+            to: user.email,
+            from: '"Support Team" <eduardo.enyoi.support@endtest-mail.io>', // override default from
+            subject: 'Recuperar contraseña',
+            template: './recovery', // `.hbs` extension is appended automatically
+            context: {
+                name: user.name,
+                url,
+            },
+        });
+    }
+}
